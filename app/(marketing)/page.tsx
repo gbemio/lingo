c0d89fcc68cfa5990974aa923c15229e06b7d694
Odukoya Gbemisola <folaryz@gmail.com>
2024-05-03 18:27:31 +0100

    Added Marketing Skeleton

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
 return (
   <div>
-<Button size="lg" variant="destructive">Click me</Button>  
+This is a marketing page! 
 </div>
 )
 }
